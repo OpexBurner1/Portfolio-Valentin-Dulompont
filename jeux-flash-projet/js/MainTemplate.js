4bbc51960b0536fe8e3c16cdd63f66fa2cfb27ba
Valentin <valentin.dulompont@gmail.com>
2021-09-06 21:49:28 +0200

    Correction de divers bugs

--- jeux-flash-projet/js/MainTemplate.js
+++ jeux-flash-projet/js/MainTemplate.js
@@ -2,13 +2,13 @@ export default Vue.component('MainTemplate', {
     template: `
         <main class="container">
             <article class ='jeux1' v-for="item of arr">
-                <img v-bind:src="'../img/' + item + '.png'" alt="">
-                <a v-bind:href="'../jeux.html/' + item + '.html'">{{item}}</a>
+                <img v-bind:src="'img/' + item + '.png'" alt="">
+                <a v-bind:href="'jeux.html/' + item + '.html'">{{item}}</a>
             </article>
         </main>`,
     data() {
         return {
-            arr: [ 'FlappyBird', 'Morpion', 'ChiFouMi', 'CasseBrique', 'Virus', 'Memory', 'Pacman' ],
+            arr: [ 'FlappyBird', 'Morpion', 'ChiFouMi', 'CasseBrique', 'Virus', 'Memory', 'pacman' ],
         };
     },
 });
